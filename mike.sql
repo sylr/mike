9513f86d8dd8e58789dbb1e1713b9e219327ad5b
Sylvain Rabot <sylvain@abstraction.fr>
2010-09-17 15:05:25 +0200

    mimetype table added
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- mike.sql
+++ mike.sql
@@ -101,6 +101,101 @@ INSERT INTO mike.inode_state (state, description) VALUES (1, 'trashed');
 INSERT INTO mike.inode_state (state, description) VALUES (2, 'waiting for physical removal');
 INSERT INTO mike.inode_state (state, description) VALUES (3, 'waiting for logical removal');
 
+-- mike.mimetype ---------------------------------------------------------------
+
+DROP TABLE IF EXISTS mike.mimetype CASCADE;
+
+CREATE TABLE mike.mimetype (
+    id_mimetype             smallint        NOT NULL PRIMARY KEY,
+    mimetype                text            NOT NULL CHECK (mimetype != ''),
+    UNIQUE (mimetype)
+) WITH (fillfactor = 98);
+
+CREATE SEQUENCE mimetype_id_mimetype_seq START 1 OWNED BY mike.mimetype.id_mimetype;
+ALTER TABLE mike.mimetype ALTER COLUMN id_mimetype SET DEFAULT nextval('mimetype_id_mimetype_seq'::regclass)::smallint;
+
+COMMENT ON TABLE mike.mimetype IS 'list of mimetypes';
+COMMENT ON COLUMN mike.mimetype.id_mimetype IS 'mimetype identifier';
+COMMENT ON COLUMN mike.mimetype.mimetype IS 'mimetype';
+
+CREATE INDEX mimetype_mimetype_btree_idx   ON mike.mimetype     USING btree (mimetype);
+
+INSERT INTO mike.mimetype (id_mimetype, mimetype) VALUES (0::smallint, 'application/x-directory');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/msword');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/octet-stream');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/ogg');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/pdf');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/pgp-signature');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/postscript');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/vnd.ms-cab-compressed');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/vnd.ms-office');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/vnd.oasis.opendocument.text');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-arc');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-archive');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-bzip2');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-coredump');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-empty');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-executable');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-freemind');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-gdbm');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-gnupg-keyring');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-gzip');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-iso9660-image');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-java-keystore');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-kdelnk');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/xml');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-object');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-pgp-keyring');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-rar');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-ruby');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-sharedlib');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-shockwave-flash');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/x-tar');
+INSERT INTO mike.mimetype (mimetype) VALUES ('application/zip');
+INSERT INTO mike.mimetype (mimetype) VALUES ('audio/mpeg');
+INSERT INTO mike.mimetype (mimetype) VALUES ('audio/x-hx-aac-adts');
+INSERT INTO mike.mimetype (mimetype) VALUES ('audio/x-wav');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/gif');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/jpeg');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/png');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/svg+xml');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/tiff');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-ico');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-ms-bmp');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-portable-bitmap');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-portable-greymap');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-portable-pixmap');
+INSERT INTO mike.mimetype (mimetype) VALUES ('image/x-xcf');
+INSERT INTO mike.mimetype (mimetype) VALUES ('message/rfc822');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/html');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/PGP');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/plain');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/rtf');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/troff');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-asm');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-awk');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-c');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-c++');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-diff');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-fortran');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-gawk');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-java');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-lisp');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-m4');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-mail');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-makefile');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-msdos-batch');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-news');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-pascal');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-perl');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-php');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-pl1');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-po');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-shellscript');
+INSERT INTO mike.mimetype (mimetype) VALUES ('text/x-tex');
+INSERT INTO mike.mimetype (mimetype) VALUES ('video/quicktime');
+INSERT INTO mike.mimetype (mimetype) VALUES ('video/x-flv');
+
 -- mike.inode ------------------------------------------------------------------
 
 DROP TABLE IF EXISTS mike.inode CASCADE;
@@ -110,13 +205,13 @@ CREATE TABLE mike.inode (
     id_inode_parent         bigint          REFERENCES mike.inode (id_inode) ON DELETE CASCADE,
     id_user                 integer         NOT NULL REFERENCES mike.user (id_user),
     state                   integer         NOT NULL DEFAULT 0 REFERENCES mike.inode_state (state),
+    id_mimetype             smallint        NOT NULL REFERENCES mike.mimetype (id_mimetype),
     name                    text            NOT NULL CHECK (name != '' AND length(name) <= 255),
     path                    text            NOT NULL CHECK (substr(path, 1, 1) = '/'),
     treepath                ltree           NOT NULL CHECK (nlevel(treepath) <= 24),
     datec                   timestamptz     NOT NULL DEFAULT now(),
     datem                   timestamptz,
     datea                   timestamptz,
-    mimetype                varchar(64),
     size                    bigint          NOT NULL DEFAULT 0,
     versioning_size         bigint          NOT NULL DEFAULT 0
 ) WITH (fillfactor = 90);
@@ -132,15 +227,15 @@ COMMENT ON COLUMN mike.inode.treepath IS 'treepath of the inode';
 COMMENT ON COLUMN mike.inode.datec IS 'creation timestamp with timezone of the inode';
 COMMENT ON COLUMN mike.inode.datem IS 'last modification timestamp with timezone of the inode';
 COMMENT ON COLUMN mike.inode.datea IS 'last access timestamp with timezone of the inode';
-COMMENT ON COLUMN mike.inode.mimetype IS 'mimetype of the inode';
+COMMENT ON COLUMN mike.inode.id_mimetype IS 'mimetype of the inode';
 COMMENT ON COLUMN mike.inode.size IS 'size of the inode';
 COMMENT ON COLUMN mike.inode.versioning_size IS 'versioning size of the inode';
 
 CREATE INDEX inode_id_inode_btree_idx           ON mike.inode   USING btree (id_inode)          WITH (fillfactor = 95);
 CREATE INDEX inode_id_inode_parent_btree_idx    ON mike.inode   USING btree (id_inode_parent)   WITH (fillfactor = 95);
 CREATE INDEX inode_id_user_btree_idx            ON mike.inode   USING btree (id_user)           WITH (fillfactor = 95);
+CREATE INDEX inode_id_mimetype_btree_idx        ON mike.inode   USING btree (id_mimetype)       WITH (fillfactor = 95);
 CREATE INDEX inode_name_btree_idx               ON mike.inode   USING btree (name)              WITH (fillfactor = 95);
-CREATE INDEX inode_mimetype_btree_idx           ON mike.inode   USING btree (mimetype)          WITH (fillfactor = 95);
 CREATE INDEX inode_datec_btree_idx              ON mike.inode   USING btree (datec)             WITH (fillfactor = 95);
 CREATE INDEX inode_datem_btree_idx              ON mike.inode   USING btree (datem)             WITH (fillfactor = 95);
 CREATE INDEX inode_path_btree_idx               ON mike.inode   USING btree (path)              WITH (fillfactor = 95);
@@ -153,7 +248,7 @@ DROP TABLE IF EXISTS mike.directory CASCADE;
 CREATE TABLE mike.directory (
     id_inode                bigint      NOT NULL PRIMARY KEY,
     id_inode_parent         bigint      REFERENCES mike.directory (id_inode) ON DELETE CASCADE,
-    mimetype                varchar(64) NOT NULL DEFAULT 'mike/x-folder',
+    id_mimetype             smallint        NOT NULL REFERENCES mike.mimetype (id_mimetype) DEFAULT 0::smallint,
     inner_datem             timestamptz,
     inner_size              bigint      NOT NULL DEFAULT 0,
     inner_versioning_size   bigint      NOT NULL DEFAULT 0,
@@ -168,6 +263,7 @@ COMMENT ON TABLE mike.directory IS 'table containing all the directory inodes';
 COMMENT ON COLUMN mike.directory.id_inode IS 'inode unique identifier';
 COMMENT ON COLUMN mike.directory.id_inode_parent IS 'identifier of parent inode';
 COMMENT ON COLUMN mike.directory.id_user IS 'owner of the inode';
+COMMENT ON COLUMN mike.directory.id_mimetype IS 'mimetype of the inode';
 COMMENT ON COLUMN mike.directory.state IS 'state of the inode, references mike.inode_state';
 COMMENT ON COLUMN mike.directory.name IS 'name of the inode, limited to 256 characters';
 COMMENT ON COLUMN mike.directory.path IS 'path of the inode';
@@ -176,7 +272,6 @@ COMMENT ON COLUMN mike.directory.datec IS 'creation timestamp with timezone of t
 COMMENT ON COLUMN mike.directory.datem IS 'last modification timestamp with timezone of the inode';
 COMMENT ON COLUMN mike.directory.inner_datem IS 'modification date of last updated child directory';
 COMMENT ON COLUMN mike.directory.datea IS 'last access timestamp with timezone of the inode';
-COMMENT ON COLUMN mike.directory.mimetype IS 'mimetype of the inode';
 COMMENT ON COLUMN mike.directory.size IS 'size in bytes of the inode';
 COMMENT ON COLUMN mike.directory.inner_size IS 'size sum of child directories';
 COMMENT ON COLUMN mike.directory.versioning_size IS 'size in bytes of the inode';
@@ -209,6 +304,7 @@ COMMENT ON TABLE mike.file IS 'table containing all the file inodes';
 COMMENT ON COLUMN mike.file.id_inode IS 'inode unique identifier';
 COMMENT ON COLUMN mike.file.id_inode_parent IS 'identifier of parent inode';
 COMMENT ON COLUMN mike.file.id_user IS 'owner of the inode';
+COMMENT ON COLUMN mike.file.id_mimetype IS 'mimetype of the inode';
 COMMENT ON COLUMN mike.file.state IS 'state of the inode, references mike.inode_state';
 COMMENT ON COLUMN mike.file.name IS 'name of the inode, limited to 256 characters';
 COMMENT ON COLUMN mike.file.path IS 'path of the inode';
@@ -216,15 +312,14 @@ COMMENT ON COLUMN mike.file.treepath IS 'treepath of the inode';
 COMMENT ON COLUMN mike.file.datec IS 'creation timestamp with timezone of the inode';
 COMMENT ON COLUMN mike.file.datem IS 'last modification timestamp with timezone of the inode';
 COMMENT ON COLUMN mike.file.datea IS 'last access timestamp with timezone of the inode';
-COMMENT ON COLUMN mike.file.mimetype IS 'mimetype of the inode';
 COMMENT ON COLUMN mike.file.size IS 'size of the inode';
 COMMENT ON COLUMN mike.file.versioning_size IS 'versioning size of the inode';
 
 CREATE INDEX file_id_inode_btree_idx            ON mike.file    USING btree (id_inode)          WITH (fillfactor = 95);
 CREATE INDEX file_id_inode_parent_btree_idx     ON mike.file    USING btree (id_inode_parent)   WITH (fillfactor = 95);
 CREATE INDEX file_id_user_btree_idx             ON mike.file    USING btree (id_user)           WITH (fillfactor = 95);
+CREATE INDEX file_id_mimetype_btree_idx         ON mike.file    USING btree (id_mimetype)       WITH (fillfactor = 95);
 CREATE INDEX file_name_btree_idx                ON mike.file    USING btree (name)              WITH (fillfactor = 95);
-CREATE INDEX file_mimetype_btree_idx            ON mike.file    USING btree (mimetype)          WITH (fillfactor = 95);
 CREATE INDEX file_datec_btree_idx               ON mike.file    USING btree (datec)             WITH (fillfactor = 95);
 CREATE INDEX file_datem_btree_idx               ON mike.file    USING btree (datem)             WITH (fillfactor = 95);
 CREATE INDEX file_path_btree_idx                ON mike.file    USING btree (path)              WITH (fillfactor = 95);
@@ -278,6 +373,7 @@ DROP TABLE IF EXISTS mike.xfile CASCADE;
 CREATE TABLE mike.xfile (
     id_xfile                bigserial       NOT NULL PRIMARY KEY,
     id_volume               integer         NOT NULL REFERENCES mike.volume (id_volume) ON DELETE CASCADE,
+    id_mimetype             smallint        NOT NULL REFERENCES mike.mimetype (id_mimetype) ON DELETE RESTRICT,
     size                    bigint          NOT NULL,
     sha1                    text            CHECK (length(sha1) = 40),
     md5                     text            CHECK (length(md5) = 40)
