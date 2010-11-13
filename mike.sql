ca28fd9f68cb53bf35c6863d6419f74174b680d9
Sylvain Rabot <sylvain@abstraction.fr>
2010-11-13 20:56:28 +0100

    file_count and dir_count made smallint
    That should be enough.
    
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- mike.sql
+++ mike.sql
@@ -180,9 +180,9 @@ CREATE TABLE mike.directory (
     inner_datem             timestamptz,
     inner_size              bigint          NOT NULL DEFAULT 0,
     inner_versioning_size   bigint          NOT NULL DEFAULT 0,
-    dir_count               integer         NOT NULL DEFAULT 0,
+    dir_count               smallint        NOT NULL DEFAULT 0,
     inner_dir_count         integer         NOT NULL DEFAULT 0,
-    file_count              integer         NOT NULL DEFAULT 0,
+    file_count              smallint        NOT NULL DEFAULT 0,
     inner_file_count        integer         NOT NULL DEFAULT 0,
     UNIQUE (id_inode_parent, name)
 ) INHERITS (mike.inode) WITH (fillfactor = 90);
