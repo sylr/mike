2935c84c0bac1dca9dead2e4edbce36caf639b9a
Sylvain Rabot <sylvain@abstraction.fr>
2010-11-13 22:29:23 +0100

    rmdir improved
    Made working correctly and improved dereferencing.
    
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- functions/rmdir.sql
+++ functions/rmdir.sql
@@ -16,16 +16,35 @@ CREATE OR REPLACE FUNCTION mike.rmdir(
 
 DECLARE
     v_directory             mike.directory%rowtype;
+    v_directory_parent      mike.directory%rowtype;
 BEGIN
-    -- select id_inode
+    -- select directory
     SELECT * INTO v_directory FROM mike.directory WHERE id_inode = in_id_inode AND id_user = in_id_user;
-    IF NOT FOUND THEN RAISE EXCEPTION 'in_id_inode #% owned by #% not found', in_id_inode, in_id_user; END IF;
+    IF NOT FOUND THEN RAISE EXCEPTION 'directory #% owned by #% not found', in_id_inode, in_id_user; END IF;
 
-    -- update id_inode
+    -- directory already removed
+    IF v_directory.state >= 2 THEN
+        RAISE EXCEPTION 'directory #% already removed', in_id_inode;
+    END IF;
+
+    -- select parent directory
+    SELECT * INTO v_directory_parent FROM mike.directory WHERE id_inode = v_directory.id_inode_parent AND id_user = in_id_user;
+
+    -- dereferencing directory
     UPDATE mike.directory SET id_inode_parent = id_inode WHERE id_inode = in_id_inode;
 
     -- update children state to 'waiting for physical removal'
-    UPDATE mike.inode SET state = 2 WHERE treepath <@ v_directory.treepath;
+    UPDATE mike.inode SET
+        state       = 2,
+        path        = '/' || v_directory.name || substring(path, length(v_directory.path) + 1),
+        treepath    = subpath(treepath, nlevel(v_directory.treepath) - 1),
+        datem       = now()
+    WHERE treepath <@ v_directory.treepath;
+
+    -- directory removed is a root folder we stop here
+    IF v_directory.id_inode = v_directory.id_inode_parent THEN
+        RETURN;
+    END IF;
 
     -- update id_inode_parent
     UPDATE mike.directory SET
@@ -37,7 +56,12 @@ BEGIN
         datem                   = greatest(datem, now()),
         inner_datem             = greatest(inner_datem, now())
     WHERE
-        id_inode_parent = v_directory.id_inode_parent;
+        id_inode = v_directory.id_inode_parent;
+
+    -- parent directory removed is a root folder we stop here
+    IF v_directory_parent.id_inode = v_directory_parent.id_inode_parent THEN
+        RETURN;
+    END IF;
 
     -- update ancestors metadata
     UPDATE mike.directory SET
@@ -55,4 +79,4 @@ $__$ LANGUAGE plpgsql VOLATILE;
 COMMENT ON FUNCTION mike.rmdir(
     IN  in_id_user          bigint,
     IN  in_id_inode         bigint
-) IS 'this function flags a directory and all its children as removed';
+) IS 'this function flags a directory and all its children inodes as removed';
