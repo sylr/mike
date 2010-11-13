5d61d26779d17a9509384df3fa8e60c3acc62aaf
Sylvain Rabot <sylvain@abstraction.fr>
2010-11-13 22:29:45 +0100

    mkdir debug
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- functions/mkdir.sql
+++ functions/mkdir.sql
@@ -67,12 +67,13 @@ CREATE OR REPLACE FUNCTION mike.mkdir(
 ) RETURNS bigint AS $__$
 
 DECLARE
+    v_id_inode      bigint;
     v_directory     mike.directory%rowtype;
     v_treepath      ltree;
 BEGIN
     -- check name unicity
-    SELECT id_inode FROM inode WHERE id_inode_parent = in_id_inode_parent AND name = in_name;
-    IF NOT FOUND THEN RAISE EXCEPTION 'inode name ''%'' already exists in id_inode_parent #%', in_name, in_id_inode_parent; END IF;
+    SELECT id_inode INTO v_id_inode FROM inode WHERE id_inode_parent = in_id_inode_parent AND name = in_name;
+    IF FOUND THEN RAISE EXCEPTION 'inode name ''%'' already exists in id_inode_parent #%', in_name, in_id_inode_parent; END IF;
 
     -- select id_inode
     SELECT nextval('inode_id_inode_seq'::regclass) INTO out_id_inode;
