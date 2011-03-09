3ba995c946b8e231e9de3044017ad681aec657d3
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-10 00:06:00 +0100

    contrib: add query which generate lot of users
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ contrib/sql/mike-gen-lot-of-users.sql
@@ -0,0 +1,31 @@
+-- Mike's Misc
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 09/03/2011
+-- copyright: All rights reserved
+
+INSERT INTO mike.user (id_user_sso, nickname)
+WITH last_user_seq AS (
+    (
+        SELECT 0::bigint AS last
+    )
+    UNION
+    (
+        SELECT
+            substring(id_user_sso, 5)::bigint AS last
+        FROM mike.user
+        WHERE
+            id_user_sso LIKE 'mike-%'
+        ORDER BY __natsort(id_user_sso) DESC
+        LIMIT 1
+    )
+    ORDER BY last DESC
+    LIMIT 1
+)
+SELECT
+    'mike-' || generate_series,
+    'mike-' || generate_series
+FROM generate_series(
+    (SELECT last + 1 FROM last_user_seq),
+    (SELECT last + 100 FROM last_user_seq)
+);
