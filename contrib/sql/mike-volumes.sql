6337cc78349a1b9b156bde882b82abae60199d0c
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-10 00:04:03 +0100

    contrib: add query to fill volumes
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ contrib/sql/mike-volumes.sql
@@ -0,0 +1,15 @@
+-- Mike's Misc
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 09/03/2011
+-- copyright: All rights reserved
+
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas01/enclosure01/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas01/enclosure02/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas01/enclosure03/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas02/enclosure01/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas02/enclosure02/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas02/enclosure03/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas03/enclosure01/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas03/enclosure02/', 17592186044416, 0);
+INSERT INTO volume (path, max_size, state) VALUES ('/exports/nas03/enclosure03/', 17592186044416, 0);
