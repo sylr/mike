960ea6aad68ca1aa006663ef51355048ed088b9a
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-10 23:01:49 +0100

    data: add file with default configuration
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ data/conf.sql
@@ -0,0 +1,7 @@
+-- Mike's Configuration
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 10/03/2011
+-- copyright: All rights reserved
+
+SELECT * FROM mike.__set_conf('tree_max_depth', '24');
