sylr/mike

commit 7fa35d8e56927ea2c13eaa5d9604f1ad3c2893f0

functions: update ls to improve planing of stable function calls
Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

functions/ls.sql
@@ -21,7 +21,7 @@ CREATE OR REPLACE FUNCTION mike.ls(
         id_inode_parent,
         id_user,
         state,
-        mike.__get_mimetype(id_mimetype) AS mimetype,
+        mike.__get_mimetype(0) AS mimetype,
         name,
         path,
         treepath,
@@ -105,7 +105,7 @@ SELECT * FROM (
             id_inode_parent,
             id_user,
             state,
-            mike.__get_mimetype(id_mimetype) AS mimetype,
+            mike.__get_mimetype(0) AS mimetype,
             name,
             path,
             treepath,
@@ -199,7 +199,7 @@ BEGIN
                     id_inode_parent,
                     id_user,
                     state,
-                    mike.__get_mimetype(id_mimetype) AS mimetype,
+                    mike.__get_mimetype(0) AS mimetype,
                     name,
                     path,
                     treepath,
@@ -295,7 +295,7 @@ BEGIN
                     id_inode_parent,
                     id_user,
                     state,
-                    mike.__get_mimetype(id_mimetype) AS mimetype,
+                    mike.__get_mimetype(0) AS mimetype,
                     name,
                     path,
                     treepath,

commit 534aaa6fc52f5bcb9eca812662152a0a2681a92d

functions: fix bug introduced in 7fa35d8e
smallint cast is mandatory in order to make the function signature right.

Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

functions/ls.sql
@@ -14,6 +14,7 @@ CREATE OR REPLACE FUNCTION mike.ls(
     IN  in_id_inode             bigint
 ) RETURNS SETOF mike.inode_full_t AS $__$
 
+-- PREPARE ls(int, bigint) AS
 (
     -- directories
     SELECT
@@ -21,7 +22,7 @@ CREATE OR REPLACE FUNCTION mike.ls(
         id_inode_parent,
         id_user,
         state,
-        mike.__get_mimetype(0) AS mimetype,
+        mike.__get_mimetype(0::smallint) AS mimetype,
         name,
         path,
         treepath,
@@ -105,7 +106,7 @@ SELECT * FROM (
             id_inode_parent,
             id_user,
             state,
-            mike.__get_mimetype(0) AS mimetype,
+            mike.__get_mimetype(0::smallint) AS mimetype,
             name,
             path,
             treepath,
@@ -199,7 +200,7 @@ BEGIN
                     id_inode_parent,
                     id_user,
                     state,
-                    mike.__get_mimetype(0) AS mimetype,
+                    mike.__get_mimetype(0::smallint) AS mimetype,
                     name,
                     path,
                     treepath,
@@ -295,7 +296,7 @@ BEGIN
                     id_inode_parent,
                     id_user,
                     state,
-                    mike.__get_mimetype(0) AS mimetype,
+                    mike.__get_mimetype(0::smallint) AS mimetype,
                     name,
                     path,
                     treepath,