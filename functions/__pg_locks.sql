0ab943fd8a0a5e32165624c1a6e77ecf0a534984
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-23 19:05:41 +0100

    funtions: add function which shows database locks
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ functions/__pg_locks.sql
@@ -0,0 +1,51 @@
+-- Mike's Function
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 23/03/2011
+-- copyright: All rights reserved
+
+DROP TYPE IF EXISTS __pg_locks_t CASCADE;
+
+CREATE TYPE __pg_locks_t AS (
+    datname         name,
+    relname         name,
+    locktype        text,
+    mode            text,
+    granted         boolean,
+    pid             integer,
+    transactionid   xid
+);
+
+DROP FUNCTION IF EXISTS mike.__pg_locks(
+) CASCADE;
+
+CREATE OR REPLACE FUNCTION mike.__pg_locks(
+) RETURNS SETOF __pg_locks_t AS $__$
+
+SELECT
+    pg_database.datname,
+    pg_class.relname,
+    pg_locks.locktype,
+    pg_locks.mode,
+    pg_locks.granted,
+    pg_locks.pid,
+    pg_locks.transactionid
+FROM
+    pg_locks
+LEFT JOIN
+    pg_class ON (pg_locks.relation = pg_class.oid)
+LEFT JOIN
+    pg_database ON (pg_locks.database = pg_database.oid)
+WHERE
+    datname IS NULL OR
+    datname =  current_database()
+ORDER BY
+    datname,
+    mode,
+    relname,
+    pid;
+
+$__$ LANGUAGE sql STABLE;
+
+COMMENT ON FUNCTION mike.__pg_locks(
+) IS 'show databases locks';
--- functions/functions.mak
+++ functions/functions.mak
@@ -15,6 +15,7 @@ DATABASE_FUNCTIONS  += __make_lot_of_directories.sql
 DATABASE_FUNCTIONS  += __make_tree.sql
 DATABASE_FUNCTIONS  += __natsort.pl
 DATABASE_FUNCTIONS  += __perform.sql
+DATABASE_FUNCTIONS  += __pg_locks.sql
 DATABASE_FUNCTIONS  += adduser.sql
 DATABASE_FUNCTIONS  += ls.sql
 DATABASE_FUNCTIONS  += mkdir.sql
