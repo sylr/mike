918a28535cb72a2984d1c04f00e64dfc54e87ed5
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-23 17:38:32 +0100

    contrib: add auto_explain contrib for analyze
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ contrib/sql/mike-auto-explain.sql
@@ -0,0 +1,12 @@
+-- Mike's Misc
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 23/03/2011
+-- copyright: All rights reserved
+
+LOAD 'auto_explain';
+SET auto_explain.log_min_duration       = 1;
+SET auto_explain.log_analyze            = true;
+SET auto_explain.log_nested_statements  = true;
+SET client_min_messages                 = 'log';
+SET log_min_messages                    = 'log';
