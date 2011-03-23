be69d84a742c3c3dacca7ec1f812de6041691bc5
Sylvain Rabot <sylvain@abstraction.fr>
2011-03-23 23:38:21 +0100

    functions: add option to __make_tree to disable version
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- functions/__make_tree.sql
+++ functions/__make_tree.sql
@@ -7,7 +7,8 @@
 CREATE OR REPLACE FUNCTION mike.__make_tree(
     in_id_user          integer,
     in_dirs_by_level    integer[] DEFAULT ARRAY[5, 5, 5],
-    in_files_by_level   integer[] DEFAULT ARRAY[5, 5, 5]
+    in_files_by_level   integer[] DEFAULT ARRAY[5, 5, 5],
+    in_versioning       boolean   DEFAULT true
 ) RETURNS void AS $__$
 
 DECLARE
@@ -26,7 +27,7 @@ DECLARE
     v_sha1              text;
     v_rand              integer;
     v_mimetype          smallint;
-    v_mimetypes         text[] := ARRAY['text/plain', 'image/jpg', 'audio/x-flac', 'text/x-shellscript', 'video/mkv'];
+    v_mimetypes         text[] := ARRAY['text/plain', 'image/jpeg', 'audio/x-flac', 'text/x-shellscript', 'video/mkv'];
     v_extension         text;
     v_extensions        text[] := ARRAY['txt', 'jpg', 'flac', 'sh', 'mkv'];
 BEGIN
@@ -59,8 +60,8 @@ BEGIN
                     FOR v_ijkl IN SELECT generate_series(0, in_files_by_level[v_i] - 1) LOOP
                         v_rand      := (random() * 1000)::int;
                         v_size      := (random() * 1024 * 1024 * 17)::bigint;
-                        v_mimetype  := out_id_mimetype FROM mike.__get_id_mimetype(v_mimetypes[v_rand  % 4 + 1]);
-                        v_extension := v_extensions[v_rand  % 4 + 1];
+                        v_mimetype  := out_id_mimetype FROM mike.__get_id_mimetype(v_mimetypes[v_rand  % 5 + 1]);
+                        v_extension := v_extensions[v_rand  % 5 + 1];
                         v_md5       := encode(digest(random()::text, 'md5'), 'hex');
                         v_sha1      := encode(digest(random()::text, 'sha1'), 'hex');
 
@@ -68,7 +69,7 @@ BEGIN
                         SELECT out_id_xfile INTO v_id_xfile     FROM mike.xtouch(v_size, v_mimetype, v_md5, v_sha1);
                         PERFORM mike.xlink(v_id_inode_f, v_id_xfile);
 
-                        IF random()::integer = 1 THEN
+                        IF in_versioning AND random()::integer = 1 THEN
                             v_size      := (random() * 1024 * 1024 * 17)::bigint;
                             v_md5       := encode(digest(random()::text, 'md5'), 'hex');
                             v_sha1      := encode(digest(random()::text, 'sha1'), 'hex');
@@ -88,7 +89,8 @@ $__$ LANGUAGE plpgsql VOLATILE;
 COMMENT ON FUNCTION mike.__make_tree(
     in_id_user          integer,
     in_dirs_by_level    int[],
-    in_files_by_level   int[]
+    in_files_by_level   int[],
+    in_versioning       boolean
 ) IS 'create a tree on several level';
 
 --------------------------------------------------------------------------------
@@ -96,7 +98,8 @@ COMMENT ON FUNCTION mike.__make_tree(
 CREATE OR REPLACE FUNCTION mike.__make_tree(
     in_id_user          integer,
     in_nb_level         integer DEFAULT 2,
-    in_inode_by_level   integer DEFAULT 10
+    in_inode_by_level   integer DEFAULT 10,
+    in_versioning       boolean DEFAULT true
 ) RETURNS void AS $__$
 
 SELECT * FROM mike.__make_tree(
@@ -110,5 +113,6 @@ $__$ LANGUAGE SQL VOLATILE;
 COMMENT ON FUNCTION mike.__make_tree(
     in_id_user          integer,
     in_dirs_by_level    int,
-    in_files_by_level   int
+    in_files_by_level   int,
+    in_versioning       boolean
 ) IS 'create a tree on several level with a fixed length of inodes by level';
