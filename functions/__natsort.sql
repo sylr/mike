bc8c6f90064079008dec0b00a559273e73360f78
Sylvain Rabot <sylvain@abstraction.fr>
2011-04-30 18:28:43 +0200

    functions: fix DATABASE_INSTALLED_SO path
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- functions/__natsort.sql
+++ functions/__natsort.sql
@@ -7,21 +7,13 @@
 CREATE OR REPLACE FUNCTION mike.__natsort(
     in_text         text
 ) RETURNS text AS
-#ifndef DEV_MODE
-    '$libdir/mike.so', '__natsort'
-#else
     DATABASE_INSTALLED_SO, '__natsort'
-#endif
 LANGUAGE C STRICT IMMUTABLE COST 1000;
 
 #ifndef X86_64
 CREATE OR REPLACE FUNCTION mike.__natsort_asm(
     in_text         text
 ) RETURNS text AS
-#ifndef DEV_MODE
-    '$libdir/mike.so', '__natsort_asm'
-#else
     DATABASE_INSTALLED_SO, '__natsort_asm'
-#endif
 LANGUAGE C STRICT IMMUTABLE COST 1000;
 #endif
