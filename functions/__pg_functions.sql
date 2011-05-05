d2e070f6cc690b1a2de3217ecff2e769043a1bbd
Sylvain Rabot <sylvain@abstraction.fr>
2011-05-05 23:20:03 +0200

    functions: move function out of mike-functions contrib
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

---
+++ functions/__pg_functions.sql
@@ -0,0 +1,28 @@
+-- Mike's Function
+-- vim: set tabstop=4 expandtab autoindent smartindent:
+-- author: Sylvain Rabot <sylvain@abstraction.fr>
+-- date: 04/03/2011
+-- copyright: All rights reserved
+
+DROP TYPE IF EXISTS mike.__pg_functions_t CASCADE;
+
+CREATE TYPE mike.__pg_functions_t AS (
+    "oid"           oid,
+    "name"          name,
+    arguments       text
+);
+
+CREATE OR REPLACE FUNCTION mike.__pg_functions(
+) RETURNS SETOF __pg_functions_t AS $__$
+
+SELECT
+    p.oid AS oid,
+    p.proname AS name,
+    pg_catalog.pg_get_function_identity_arguments(p.oid) AS arguments,
+FROM pg_catalog.pg_proc p
+LEFT JOIN pg_catalog.pg_namespace n ON n.oid = p.pronamespace
+WHERE
+    n.nspname ~ '^(mike)$'
+ORDER BY 1, 2;
+
+$__$ LANGUAGE sql;
