e64096c60f8b9cad21d3bd36552e0be7e5ee35d2
Sylvain Rabot <sylvain@abstraction.fr>
2011-06-15 18:34:07 +0200

    functions: improve __get_least_used_lv
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- functions/__get_least_used_lv.sql
+++ functions/__get_least_used_lv.sql
@@ -13,8 +13,37 @@ DECLARE
     v_user_count    integer;
     v_return        text;
 BEGIN
+    IF 'user_count' = mike.__get_conf('lv_repartition_mode', false) THEN
+        SELECT
+            lv.name INTO v_return
+        FROM
+            mike.lv
+        ORDER BY
+            coalesce(array_length(users, 1), 0) ASC,
+            random()
+        LIMIT 1;
+
+        RETURN v_return;
+    ELSIF 'liv_tup' = mike.__get_conf('lv_repartition_mode', false) THEN
+       SELECT
+            lv.name INTO v_return
+        FROM
+            mike.lv
+        JOIN pg_stat_user_tables ON (relname = 'file_' || lv.name OR relname = 'directory_' || lv.name)
+        GROUP BY
+            lv.name
+        ORDER BY
+            sum(n_live_tup),
+            random()
+        ASC LIMIT 1;
+
+        RETURN v_return;
+    END IF;
+
+    -- user count
     SELECT count(id_user) INTO v_user_count FROM mike.user;
 
+    -- default methods if no configuration set
     -- if we have less than an hundred users we user user count by lv to return least
     -- used lv, otherwise we used the sum of tuples of file_<lv> and directory_<lv>
     IF v_user_count < 100 THEN
@@ -23,7 +52,8 @@ BEGIN
         FROM
             mike.lv
         ORDER BY
-            coalesce(array_length(users, 1), 0) ASC
+            coalesce(array_length(users, 1), 0) ASC,
+            random()
         LIMIT 1;
     ELSE
        SELECT
@@ -34,7 +64,9 @@ BEGIN
         GROUP BY
             lv.name
         ORDER BY
-            sum(n_live_tup) ASC LIMIT 1;
+            sum(n_live_tup),
+            random()
+        ASC LIMIT 1;
     END IF;
 
     RETURN v_return;
