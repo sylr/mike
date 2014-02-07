2bb79c244ce80b89c3ddbf372ec511ce8985a4bc
Sylvain Rabot <sylvain@abstraction.fr>
2014-02-07 22:07:14 +0100

    sql: Fix pg_size_pretty call
    On Postgresql 9.1 pg_size_pretty(numeric) does not exist.
    
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- views/__pg_relbloat.sql
+++ views/__pg_relbloat.sql
@@ -35,7 +35,11 @@ SELECT
     ROUND(CASE WHEN iotta = 0 OR ipages = 0 THEN 0.0 ELSE ipages / iotta::numeric END, 1) AS ibloat,
     CASE WHEN ipages < iotta THEN 0 ELSE ipages::bigint - iotta END AS wastedipages,
     CASE WHEN ipages < iotta THEN 0 ELSE bs * (ipages - iotta) END AS wastedibytes,
+#ifdef PG_VERSION_9_2_PLUS
     CASE WHEN ipages < iotta THEN pg_size_pretty(0::numeric) ELSE pg_size_pretty((bs * (ipages - iotta))::bigint) END AS wastedisize
+#else
+    CASE WHEN ipages < iotta THEN pg_size_pretty(0) ELSE pg_size_pretty((bs * (ipages - iotta))::bigint) END AS wastedisize
+#endif
 FROM (
     SELECT
         schemaname,
